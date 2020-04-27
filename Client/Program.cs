422bab7a58de4fc7248ad06c74aeae875a4578e1
Szilvia <szilviakovacs55@gmail.com>
2020-04-27 23:32:35 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,93 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Client
+{
+    class Program
+    {
+
+
+        public static bool isDateValid(string date)
+        {
+            var dateRegex = new Regex(@"\b(((0?[469]|11)/(0?[1-9]|[12]\d|30)|(0?[13578]|1[02])/(0?[1-9]|[12]\d|3[01])|0?2/(0?[1-9]|1\d|2[0-8]))/([1-9]\d{3}|\d{2})|0?2/29/([1-9]\d)?([02468][048]|[13579][26]))\b", RegexOptions.IgnorePatternWhitespace);
+            return (dateRegex.Match(date).Success) ? true : false;
+        }
+
+        public static void shortenDate(ref string date)
+        {
+            if (date.Split('/')[0][0] == '0')
+            {
+                string[] input = date.Split('/');
+                date = input[0].Substring(input[0].Length - 1) + "/" + input[1] + "/" + input[2];
+            }
+
+            if (date.Split('/')[1][0] == '0')
+            {
+                string[] input = date.Split('/');
+                date = input[0] + "/" + input[1].Substring(input[1].Length - 1) + "/" + input[2];
+            }
+
+            return;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+           /* 
+
+
+            const string Host = "localhost";
+            const int Port = 16842;
+
+            var channel = new Channel($"{Host}:{Port}", ChannelCredentials.Insecure);
+
+
+            shortenDate(ref date);
+
+            var client = new Generated.ZodiacService.ZodiacServiceClient(channel);
+
+            var response = client.Zodiac(new Generated.ZodiacRequest
+            {
+                Message = date
+            });
+
+            Console.WriteLine("\nReceived response:", response.Response);*/
+
+            const string Host = "localhost";
+            const int Port = 16842;
+
+            var channel = new Channel($"{Host}:{Port}", ChannelCredentials.Insecure);
+
+            string date;
+            do
+            {
+                Console.WriteLine("Enter a date: [format: mm/dd/yyyy]");
+                date = Console.ReadLine();
+            } while (!isDateValid(date)); 
+
+            shortenDate(ref date);
+
+            var client = new Generated.ZodiacService.ZodiacServiceClient(channel);
+
+            var response = client.Zodiac(new Generated.ZodiacRequest
+            {
+                Message = date
+            });
+
+            Console.WriteLine("\n Your zodiac sign is: {0}", response.Response);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+   
+
---
+++ Server/ZodiacService.cs
@@ -0,0 +1,50 @@
+﻿using Generated;
+using Grpc.Core;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+using System;
+
+namespace Server
+{
+    internal class ZodiacService : Generated.ZodiacService.ZodiacServiceBase
+    {
+
+        private string getHoroscope(string date)
+        {
+            FileStream file = File.OpenRead("../../../input.txt");
+            StreamReader streamReader = new StreamReader(file);
+
+            List<string[]> horoscopeList = new List<string[]>();
+            string line, result = "";
+
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                horoscopeList.Add(line.Split(' '));
+            }
+
+            string[] inputDate = date.Split('/');
+
+            for (int i= 0; i < horoscopeList.Count; ++i)
+            {
+                if ((horoscopeList[i][0].Split('/')[0] == inputDate[0] && Int32.Parse(inputDate[1]) >= Int32.Parse(horoscopeList[i][0].Split('/')[1])) 
+                    ||
+                    (horoscopeList[i][1].Split('/')[0] == inputDate[0] && Int32.Parse(inputDate[1]) <= Int32.Parse(horoscopeList[i][1].Split('/')[1]))) 
+                {
+                    result = horoscopeList[i][2];
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public override Task<ZodiacResponse> Zodiac(ZodiacRequest request, ServerCallContext context)
+        {
+            var result = getHoroscope(request.Message);
+
+            System.Console.WriteLine("Date requested:", request.Message);
+
+            return Task.FromResult(new ZodiacResponse() { Response = result });
+        }
+    }
+}
